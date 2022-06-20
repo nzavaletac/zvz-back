1b89654ddde0be96628bf04c6fb989ea77642bd3
Noe Zavaleta C <nzavaletac@gmail.com>
2022-06-19 22:45:39 -0500

    database successfully connected with mongo atlas

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { connect } = require("./database");
 
 const port = process.env.PORT || 8000;
 const app = express();
+connect();
 
 app.use(express.json({ limit: "100mb" }));
 app.use(cors());
