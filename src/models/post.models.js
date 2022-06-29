f4f289dc1888b12c09b0f6f42ec619a9e44e856e
Noe Zavaleta C <nzavaletac@gmail.com>
2022-06-28 23:12:39 -0500

    post models is finished

---
+++ src/models/post.models.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const { marked } = require("marked");
+const slugify = require("slugify");
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  markdowon: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
+});
+
+postSchema.pre("validate", function (next) {
+  if (this.tile) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  if (this.markdowon) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdowon));
+  }
+  next();
+});
+
+module.exports = model("Post", postSchema);
